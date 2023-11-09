41fa5fdf4e68034d093b17d27c5313b818515800
AlexisEstela <aestelalozano@gmail.com>
2023-11-09 15:26:39 -0500

    Agregación de autenticacion

--- app.js
+++ app.js
@@ -2,11 +2,35 @@ require('dotenv').config();
 
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
+const connectDB = require('./server/config/database')
+const session = require('express-session')
+const passport = require('passport')
+const MongoStore = require('connect-mongo')
+
 
 const app = express()
 const port = 5000 || process.env.PORT
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+
+    cookie: { maxAge: new Date( Date.now() + (3600000))}
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+
+// Connect to Database
+connectDB()
 
 
 //Static Files
@@ -19,6 +43,7 @@ app.set('layout','./layouts/main')
 app.set('view engine', 'ejs')
 
 // Routes
+app.use('/',require('./server/routes/auth'))
 app.use('/', require('./server/routes/index'))
 app.use('/',require('./server/routes/dashboard'))
 
@@ -31,3 +56,5 @@ app.get('*', function(req,res){
 app.listen(port,() =>{
     console.log(`App listening on port  ${port}`)
 })
+
+
